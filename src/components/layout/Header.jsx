d1352ac95d0a6d8324ee189723c4e8a6e2b7c1ea
Saanya <sj236@njit.edu>
2025-10-21 15:19:23 -0400

    Header Changes

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,8 +1,12 @@
 import jade_logo from '../../assets/jade_logo.png';
+import { Menu } from 'lucide-react';
 
 function Header(){
     return(
         <header className='header'>
+            <div className='menu-icon'>
+                <Menu strokeWidth={3}/>
+            </div>
             <div className='logo'>
                 <img src={jade_logo} className="logo-img" />    
             </div>
