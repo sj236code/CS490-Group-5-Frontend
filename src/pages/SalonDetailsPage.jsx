17ce0ba1bba4d79496b2a7f597688e79a739070c
Saanya <sj236@njit.edu>
2025-10-24 04:51:02 -0400

    Basic Tab Nav Implemented

--- src/pages/SalonDetailsPage.jsx
+++ src/pages/SalonDetailsPage.jsx
@@ -1,25 +1,90 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Star } from 'lucide-react';
 
 function SalonDetailsPage() {
     const location = useLocation();
+    const navigate = useNavigate();
     const { salon } = location.state || {};
-    
-    return (
-        <div style={{ padding: '50px', textAlign: 'center' }}>
-            <h1>Salon Details Page</h1>
-            
-            {salon ? (
-                <div>
-                    <p style={{ fontSize: '24px', marginTop: '20px' }}>
-                        Salon ID: <strong>{salon.id}</strong>
-                    </p>
-                    <p style={{ fontSize: '24px' }}>
-                        Salon Name: <strong>{salon.title}</strong>
-                    </p>
-                </div>
-            ) : (
+
+    const [salonDetails, setSalonDetails] = useState(salon);
+    const [workingTab, setWorkingTab] = useState("About");
+
+    // Do I have to handle possible error if no valid salon is passed?
+    useEffect(() => {
+        if (!salon){
+            console.error("No Salon Data provided.");
+        }
+        else{
+            setSalonDetails(salon);
+            console.log("Salon Details: ", salon);
+        }
+    }, [salon]);
+
+    // Add check for salonDetails
+    if (!salonDetails) {
+        return (
+            <div style={{ padding: '50px', textAlign: 'center' }}>
                 <p style={{ color: 'red' }}>No salon data found</p>
-            )}
+                <button onClick={() => navigate('/search')}>
+                    Return to Search
+                </button>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+
+            {/* Hero */}
+            <div className="salon-details-hero">
+                <div className="salon-hero-overlay"></div>
+            </div>
+
+            {/* Salon Info */}
+            <div>
+                <h1 className="salon-details-name">{salonDetails.title}</h1>
+                <div className="salon-details-info">
+                    <span style={{marginRight: '5px'}}>{salonDetails.type}</span>
+                    <span><Star size={16} fill="#96A78D"/> {salonDetails.avgRating || 'N/A'}</span>
+                    <span style={{marginLeft: '5px'}}>
+                        {salonDetails.totalReviews || 0} Reviews
+                    </span>
+                </div>
+            </div>
+
+            {/* Nvigation Tabs */}
+            <div className="salon-details-tabs">
+                <button className="salon-detail-tab-link" onClick={() => setWorkingTab('About')}>About</button>
+                <button className="salon-detail-tab-link" onClick={() => setWorkingTab('Gallery')}>Gallery</button>
+                <button className="salon-detail-tab-link" onClick={() => setWorkingTab('Reviews')}>Reviews</button>
+                <button className="salon-detail-tab-link" onClick={() => setWorkingTab('Services')}>Services</button>
+            </div>
+
+            {/* Tab Content */}
+            <div className="salon-details-tab-content">
+                {workingTab =="About" && (
+                    <div>
+                        <h2>About Page for: {salonDetails.title}</h2>
+                    </div>
+                )}
+                {workingTab =="Gallery" && (
+                    <div>
+                        <h2>Gallery Page for: {salonDetails.title}</h2>
+                    </div>
+                )}
+                {workingTab =="Reviews" && (
+                    <div>
+                        <h2>Reviews Page for: {salonDetails.title}</h2>
+                    </div>
+                )}
+                {workingTab =="Services" && (
+                    <div>
+                        <h2>Services Page for: {salonDetails.title}</h2>
+                    </div>
+                )}
+            </div>
+
         </div>
     );
 }
