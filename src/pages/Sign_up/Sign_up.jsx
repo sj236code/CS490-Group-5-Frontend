3199ca0982f4314c697d576723055202a518463a
EthanDiaz03 <luke09usa@gmail.com>
2025-10-27 17:16:32 -0400

    fixing syntax error

--- src/pages/Sign_up/Sign_up.jsx
+++ src/pages/Sign_up/Sign_up.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ChevronLeft } from 'lucide-react';
-import './SignUp.css';
+import './Sign_up.css';
 
 function SignUp() {
     const [activeTab, setActiveTab] = useState('signup');
