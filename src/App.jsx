0e81b5a1fc19e0cebc3be2c63a6ae8f1315cb1d6
HGuevara10 <hegel.guevara107@gmail.com>
2025-11-06 23:45:38 -0500

    changed file folder locations

--- src/App.jsx
+++ src/App.jsx
@@ -18,13 +18,13 @@ import RegisterSalonSuccess from './pages/Sign_up/Salon_registration_success.jsx
 import EmployeeRegistration from './pages/Sign_up/Employee_registration.jsx';
 import EmployeeRegistrationSuccess from './pages/Sign_up/Employee_registration_success.jsx';
 import ResetPassword from './pages/Sign_in/Reset_pass.jsx';
-import Checkout from './components/checkout&payment/Checkout.jsx';
+import Checkout from './pages/checkout&payment/Checkout.jsx';
 
 
 // Firebase
 import { auth } from "./firebase";
 import { onAuthStateChanged } from "firebase/auth";
-import PaymentConfirmation from './components/checkout&payment/PaymentConfirmation.jsx';
+import PaymentConfirmation from './pages/checkout&payment/PaymentConfirmation.jsx';
 
 
 function App() {
