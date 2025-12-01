4c469896e442a1e9e65d709755c348d0f2baa9cf
Saanya <sj236@njit.edu>
2025-12-01 17:20:21 -0500

    Add Employee Login Test

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -3,10 +3,10 @@
 
 import { test, expect } from '@playwright/test';
 
-// Sign into Playright Tester account
+// Sign into Playright Customer Tester account
 test('existing test user can log in', async ({ page }) => {
 
-    //Force Full Screen for Viewing Purposes
+    // Force Full Screen for Viewing Purposes
     await page.addInitScript(() => {
         window.moveTo(0, 0);
         window.resizeTo(screen.width, screen.height);
@@ -15,11 +15,32 @@ test('existing test user can log in', async ({ page }) => {
     // User clicks Sign In button
     await page.goto('/signin');
 
-    // Enter user credentials & click sign in
+    // Enter customer credentials & click sign in
     await page.getByPlaceholder('Email Address').fill('playwright_tester@jade.com');
     await page.getByPlaceholder('Password').fill('password123');
     await page.locator('form').getByRole('button', { name: 'Sign In' }).click();
 
     // Verify login succeeded
+    await expect(page).toHaveURL('/');
+});
+
+// Sign into Playwright Employee Tester account
+test('existing employee can log in', async ({ page }) => {
+
+    // Force Full Screen for Viewing Purposes
+    await page.addInitScript(() => {
+        window.moveTo(0,0);
+        window.resizeTo(screen.width, screen.height);
+    });
+
+    // User clicks Sign In button
+    await page.goto('/signin');
+
+    // Enter employee credentials & click sign in
+    await page.getByPlaceholder('Email Address').fill('playwright_tester_emp@jade.com');
+    await page.getByPlaceholder('Password').fill('password123');
+    await page.locator('form').getByRole('button', { name: 'Sign In' }).click();
+
+    // Verify login succeeses
     await expect(page).toHaveURL('/');
 });
