41264309e069b570e8377a8d751be1e8e1b9fffe
Saanya <sj236@njit.edu>
2025-12-03 14:44:59 -0500

    Fix sign in button click in login.js

--- tests/utils/login.js
+++ tests/utils/login.js
@@ -5,7 +5,7 @@ export async function loginCustomer(page) {
   await page.goto('/signin');
   await page.getByPlaceholder('Email Address').fill('playwright_tester@jade.com');
   await page.getByPlaceholder('Password').fill('password123');
-  await page.getByRole('button', { name: 'Sign In' }).click();
+  await page.locator('form').getByRole('button', { name: 'Sign In' }).click();
   await expect(page).toHaveURL('/');
 }
 
@@ -13,7 +13,7 @@ export async function loginEmployee(page) {
   await page.goto('/signin');
   await page.getByPlaceholder('Email Address').fill('playwright_tester_emp@jade.com');
   await page.getByPlaceholder('Password').fill('password123');
-  await page.getByRole('button', { name: 'Sign In' }).click();
+  await page.locator('form').getByRole('button', { name: 'Sign In' }).click();
   await expect(page).toHaveURL('/');
 }
 
@@ -21,6 +21,6 @@ export async function loginOwner(page) {
   await page.goto('/signin');
   await page.getByPlaceholder('Email Address').fill('playwright_tester_owner@jade.com');
   await page.getByPlaceholder('Password').fill('password123');
-  await page.getByRole('button', { name: 'Sign In' }).click();
+  await page.locator('form').getByRole('button', { name: 'Sign In' }).click();
   await expect(page).toHaveURL('/');
 }
